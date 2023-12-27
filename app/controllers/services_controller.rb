4532862d7ba8a707add68f1a176a91ca52f09811
Ekaterina Gaeva <106819590+Kaifutsi@users.noreply.github.com>
2023-12-27 15:32:39 +0200

    Update services_controller.rb

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,15 +1,12 @@
 class ServicesController < ApplicationController
   def index
-    # Your action logic here
-
     respond_to do |format|
-      format.html # This specifies the HTML format template
-      format.json { render json: @data } # Example: respond with JSON
+      format.html
+      format.json { render json: @data }
     end
   end
 
   def description
-    # You can fetch descriptions for different services here.
     @descriptions = {
       classic_manicure: "Classic Manicure: Our classic manicure includes...",
       french_manicure: "French Manicure: The French manicure is known for...",
@@ -18,5 +15,4 @@ class ServicesController < ApplicationController
     }
   end
 
-  # Other actions (index, show, create, etc.) as needed.
 end
